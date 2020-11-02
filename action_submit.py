ceda197f5dc480b43670cd3ab37a79a31034ac73
WangYuyang <me@echo.cool>
2020-11-02 10:11:39 +0800

    Update action_submit.py

--- action_submit.py
+++ action_submit.py
@@ -11,17 +11,13 @@ except:
     server_key = ""
 
 try:
-    username = os.environ["username"] # 自己的账号
-    password = os.environ["password"] # 自己的密码
+    usernames = os.environ["username"].split("\n")  # 账号
+    passwords = os.environ["password"].split("\n")  # 密码
 except:
     print("未获得完整用户名和密码")
 
-s = requests.Session()
-header = {"User-Agent": "Mozilla/5.0 (Linux; Android 10;  AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/6.2 TBS/045136 Mobile Safari/537.36 wxwork/3.0.16 MicroMessenger/7.0.1 NetType/WIFI Language/zh",}
-s.headers.update(header)
 
-
-def login(s: requests.Session):
+def login(s: requests.Session, username: str, password: str):
     sign_data = {
         "username": username,  # 自己的账号
         "password": password  # 自己的密码
@@ -35,7 +31,8 @@ def login(s: requests.Session):
 
 
 def get_yesterday(s: requests.Session):
-    data = s.get("https://itsapp.bjut.edu.cn/ncov/api/default/daily?xgh=0&app_id=bjut")
+    data = s.get(
+        "https://itsapp.bjut.edu.cn/ncov/api/default/daily?xgh=0&app_id=bjut")
     json_data = data.json()
     # 其中昨天数据存放于json的d中，拿不到则为 None
     d = json_data.get('d', None)
@@ -99,17 +96,22 @@ def submit(s: requests.Session, old: dict):
 
 # 微信通知
 def send_message(key, message, clock_info):
-    send_url = "https://sc.ftqq.com/{}.send?text={}&desp={}".format(key, message, clock_info)
+    send_url = "https://sc.ftqq.com/{}.send?text={}&desp={}".format(
+        key, message, clock_info)
     requests.get(send_url)
 
 
 if __name__ == "__main__":
-
-    #登录
-    login(s)
-
-    # 抓取昨天信息，用于今天提交
-    yesterday_data = get_yesterday(s)
-
-    # 提交
-    submit(s, yesterday_data)
+    if (len(usernames) != len(passwords)):
+        print("用户名密码数量不一致")
+        exit()
+    for username, password in usernames, passwords:
+        s = requests.Session()
+        header = {"User-Agent": "Mozilla/5.0 (Linux; Android 10;  AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/   6.2 TBS/045136 Mobile Safari/537.36 wxwork/3.0.16 MicroMessenger/7.0.1 NetType/WIFI Language/zh", }
+        s.headers.update(header)
+        # 登录
+        login(s, username, password)
+        # 抓取昨天信息，用于今天提交
+        yesterday_data = get_yesterday(s)
+        # 提交
+        submit(s, yesterday_data)
